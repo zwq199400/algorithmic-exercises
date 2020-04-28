ffa6d67472f1826ad95ee28e1e784ce1d0d0bfa6
周伟强 <weiqiang.zhou@xbongbong.com>
2020-04-28 19:22:06 +0800

    feat: 复制含有随机指针节点的链表

--- src/main/java/CopyWithRandom.java
+++ src/main/java/CopyWithRandom.java
@@ -26,7 +26,7 @@ public class CopyWithRandom {
         head.next.next.next.next.rand = null; // 5 -> null
         head.next.next.next.next.next.rand = head.next.next.next; // 6 -> 4
 
-        RandomNode randomNode = copyListNode(head);
+        RandomNode randomNode = copyListNode2(head);
         System.out.println("=========================");
     }
 
@@ -53,24 +53,42 @@ public class CopyWithRandom {
     }
 
     private static RandomNode copyListNode2(RandomNode listNode) {
-        //新旧节点对象映射关系
-        Map<RandomNode, RandomNode> hashNode = new HashMap<>(1 << 4);
 
         RandomNode curNode = listNode;
         while (curNode != null) {
-            RandomNode newCopyNode = new RandomNode(curNode.value);
-            hashNode.put(curNode, newCopyNode);
+            RandomNode newNode = new RandomNode(curNode.value);
+            newNode.next = curNode.next;
+            curNode.next = newNode;
+            curNode = newNode.next;
+        }
+
+        curNode = listNode;
+        int i = 1;
+        RandomNode temp = null;
+        while (curNode != null) {
+            //偶数为新拷贝的链表节点
+            if (i % 2 == 0) {
+                curNode.rand = temp;
+            } else {
+                temp = curNode.rand;
+            }
+            i++;
             curNode = curNode.next;
         }
-        RandomNode result = new RandomNode(listNode.value);
-        RandomNode temp = result;
-        while (listNode != null) {
-            temp.next = hashNode.get(listNode.next);
-            temp.rand = hashNode.get(listNode.rand);
-            listNode = listNode.next;
-            temp = temp.next;
+
+        RandomNode randomNode = new RandomNode(0);
+        RandomNode result = randomNode;
+        curNode = listNode;
+        int j = 1;
+        while (curNode != null) {
+            if (j % 2 == 0) {
+                result.next = curNode;
+                result = result.next;
+            }
+            curNode = curNode.next;
+            j++;
         }
-        return result;
+        return randomNode.next;
 
     }
 
